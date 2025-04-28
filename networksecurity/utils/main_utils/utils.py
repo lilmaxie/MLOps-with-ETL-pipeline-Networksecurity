27624793c92c64bd0ca03c286cf7ecfb776a8b2c
lilmaxie <hoangminh261003@gmail.com>
2025-04-28 21:53:25 +0700

    update utils

---
+++ networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,29 @@
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+import yaml
+import os, sys
+import numpy as np
+import dill
+import pickle
+
+def read_yaml_file(file_path: str) -> dict:
+    """Read a YAML file and return its content as a dictionary."""
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+def write_yaml_file(file_path: str, 
+                    content: object, 
+                    replace: bool = False) -> None:
+    """Write content to a YAML file."""
+    try:
+        if replace and os.path.exists(file_path):
+            os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
