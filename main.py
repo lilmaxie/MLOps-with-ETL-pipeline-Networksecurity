593c05b762c3edf1ffb9f0a5d4af4f36c33d953a
lilmaxie <hoangminh261003@gmail.com>
2025-04-30 17:04:35 +0700

    testing data transformation function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.components.data_transformation import DataTransformation
 
 import sys
 
@@ -27,6 +28,16 @@ if __name__=='__main__':
         data_validation_aritifact = data_validation.initiate_data_validation()
         logging.info("Data validation completed")
         print(data_validation_aritifact)
+        
+        data_transformation_config = DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        logging.info("Initiating data transformation")
+        data_transformation = DataTransformation(
+            data_validation_artifact=data_validation_aritifact,
+            data_transformation_config=data_transformation_config
+        )
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data transformation completed")
         
         
     except Exception as e:
