08387c2d4da3e78a943079eb27340b9f326e515c
lilmaxie <hoangminh261003@gmail.com>
2025-04-30 17:06:51 +0700

    update DataTransformationArtifact

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -15,3 +15,10 @@ class DataValidationArtifact:
     invalid_train_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+    
+@dataclass
+class DataTransformationArtifact:
+    """Data Transformation Artifact"""
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+    transformed_object_file_path: str
